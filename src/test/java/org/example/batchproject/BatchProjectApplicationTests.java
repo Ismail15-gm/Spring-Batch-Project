fa257bdae09b6724184bedd74b97993cebc58dde
Ismail Elghaba <ismail.elghaba.main@gmail.com>
2025-11-25 18:49:48 +0100

    setup

---
+++ src/test/java/org/example/batchproject/BatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.batchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
